d0287cc2b2e0fe27f3cd6cda1d088af2fd9f1ec6
Arslan <arslanmohsin000@gmail.com>
2026-09-03 02:06:23 +0500

    Revert "Complete Flutter app integration and verified changes"
    This reverts commit 0978b738be53e1b765ba6bce570d88588b800471.

--- lib/app/routes.dart
+++
@@ -1,7 +0,0 @@
-class AppRoutes {
-  static const String login = '/login';
-  static const String superAdminDashboard = '/super-admin-dashboard';
-  static const String adminSetup = '/admin-setup';
-  static const String adminDashboard = '/admin-dashboard';
-  static const String employeeDashboard = '/employee-dashboard';
-}
